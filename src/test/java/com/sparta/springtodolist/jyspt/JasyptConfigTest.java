5c1484c8d019d892201fe71979452c86e8c4e65d
junxtar <aa1870088@naver.com>
2023-11-13 22:36:18 +0900

    test: jasypt test 코드 작성

--- src/test/java/com/sparta/springtodolist/jyspt/JasyptConfigTest.java
+++ src/test/java/com/sparta/springtodolist/jyspt/JasyptConfigTest.java
@@ -1,6 +1,7 @@
 package com.sparta.springtodolist.jyspt;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class JasyptConfigTest {
@@ -10,15 +11,12 @@ public class JasyptConfigTest {
         // given
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword(" ");
-        String test = "jdbc:mysql://localhost:3306/card";
+        String test = "test";
 
-        // when, then
-        String actual = encryptor.encrypt(test);
-//        System.out.println(encryptor.encrypt("jdbc:mysql://localhost:3306/card"));
-//        System.out.println(encryptor.encrypt("root"));
-//        System.out.println(encryptor.encrypt("ekqls832@@"));
-//        Assertions.assertEquals(test, encryptor.decrypt(actual));
-        System.out.println(encryptor.decrypt("CKc/k4oHyy+uNSRS/LsuU6l/OysBk7mV"));
-//        Assertions.assertEquals("ekqls832@@", encryptor.decrypt("uf6E2yWW/snggbzESvbdqnCxJe7sKURo"));
+        // when
+        String encryptPassword = encryptor.encrypt(test);
+
+        // then
+        Assertions.assertEquals(encryptor.decrypt(encryptPassword), "test");
     }
 }
