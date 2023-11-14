0021506845ac2ccc154b67889ea7f26ed0b4e800
junxtar <aa1870088@naver.com>
2023-11-14 15:44:32 +0900

    refactor: 로그인 페이지 재구성

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
       jqXHR.setRequestHeader('Authorization', auth);
     });
   } else {
-    window.location.href = host + '/api/user/login-page';
+    window.location.href = host + '/login-page';
     return;
   }
 
@@ -24,7 +24,7 @@ $(document).ready(function () {
     const isAdmin = !!res.admin;
 
     if (!username) {
-      window.location.href = '/api/user/login-page';
+      window.location.href = '/login-page';
       return;
     }
 
@@ -424,7 +424,7 @@ function setMyprice() {
 function logout() {
   // 토큰 삭제
   Cookies.remove('Authorization', {path: '/'});
-  window.location.href = host + '/api/user/login-page';
+  window.location.href = host + '/login-page';
 }
 
 function getToken() {
