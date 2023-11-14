4be8a7d86665a80e12ea4b1e9fc2e79265cfd842
junxtar <aa1870088@naver.com>
2023-11-14 15:46:46 +0900

    feat: 기본 구성 페이지 Controller 구현

---
+++ src/main/java/com/sparta/springtodolist/domain/HomeController.java
@@ -0,0 +1,22 @@
+package com.sparta.springtodolist.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
+}
