ba254f08517bab5294d30557f51c73e9e11ed8b9
junxtar <aa1870088@naver.com>
2023-11-15 13:07:46 +0900

    test: Jasypt test code 구현

---
+++ src/test/java/com/sparta/springtodolist/jasypt/JasyptConfigTest.java
@@ -0,0 +1,22 @@
+package com.sparta.springtodolist.jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JasyptConfigTest {
+
+    @Test
+    void stringEncryptor() {
+        // given
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(" ");
+        String test = "test";
+
+        // when
+        String encryptPassword = encryptor.encrypt(test);
+
+        // then
+        Assertions.assertEquals(encryptor.decrypt(encryptPassword), "test");
+    }
+}
