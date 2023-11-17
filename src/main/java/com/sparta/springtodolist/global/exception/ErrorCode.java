cfde507c3179ebf386c46aa200bfe29965582d96
junxtar <aa1870088@naver.com>
2023-11-17 10:42:55 +0900

    feat: Not Found Username 예외 추가

--- src/main/java/com/sparta/springtodolist/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/springtodolist/global/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ErrorCode {
 
     EXISTS_USERNAME(400, "E01", "이미 존재하는 유저이름 입니다."),
-    FILED_EXCEPTION(400, "F01", "회원가입 조건에 부합합니다.");
+    FILED_EXCEPTION(400, "F01", "회원가입 조건에 부합합니다."),
+    NOT_FOUND_USERNAME(404, "U01", "존재하지 않는 유저이름 입니다.");
 
     private final int status;
     private final String code;
