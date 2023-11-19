3ff00098594e953ae38ba00ae820546dcddb6bf8
junxtar <aa1870088@naver.com>
2023-11-19 13:55:02 +0900

    clean: 코드 개선

--- src/main/java/com/sparta/springtodolist/domain/user/service/dto/request/UserSignupServiceRequestDto.java
+++ src/main/java/com/sparta/springtodolist/domain/user/service/dto/request/UserSignupServiceRequestDto.java
@@ -1,11 +1,12 @@
 package com.sparta.springtodolist.domain.user.service.dto.request;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserSignupServiceRequestDto {
 
     private String username;
