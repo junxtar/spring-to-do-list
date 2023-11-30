1a1b76aa8a5e46099c8a4a4f7bd94fc943c83dec
junxtar <aa1870088@naver.com>
2023-11-30 15:56:30 +0900

    test: 회원가입 정규식 검사 리팩토링

--- src/test/java/com/sparta/springtodolist/domain/user/regex/RegexTest.java
+++ src/test/java/com/sparta/springtodolist/domain/user/regex/RegexTest.java
@@ -1,4 +1,4 @@
-package com.sparta.springtodolist.regex;
+package com.sparta.springtodolist.domain.user.regex;
 
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.Assertions;
@@ -9,11 +9,12 @@ public class RegexTest {
 
     static final String USER_NAME_REGEXP = "^[a-z0-9]{4,10}$";
     static final String USER_PASSWORD_REGEXP = "^[a-zA-Z0-9]{8,10}$";
-    @DisplayName("유저 이름 정규식 길이 검사 실패")
+
+    @DisplayName("유저 이름 길이가 정규식에 패턴에 부합하지 않아 검사에 실패한다.")
     @Test
     void regexUsernameOfLengthFailTest() {
         // given
-        String username = "123";
+        String username = "tom";
 
         // when
         boolean isActual = Pattern.matches(USER_NAME_REGEXP, username);
@@ -22,11 +23,11 @@ public class RegexTest {
         Assertions.assertFalse(isActual);
     }
 
-    @DisplayName("유저 이름 정규식 대문자 검사 실패")
+    @DisplayName("유저 이름 대문자가 정규식에 패턴에 부합하지 않아 검사에 실패한다.")
     @Test
     void regexUsernameOfUppercaseFailTest() {
         // given
-        String username = "TEST";
+        String username = "Test";
 
         // when
         boolean isActual = Pattern.matches(USER_NAME_REGEXP, username);
@@ -35,11 +36,11 @@ public class RegexTest {
         Assertions.assertFalse(isActual);
     }
 
-    @DisplayName("유저 이름 정규식 검사 성공")
+    @DisplayName("유저 이름이 정규식에 패턴에 부합해 성공한다.")
     @Test
     void regexUsernameSuccessTest() {
         // given
-        String username = "abc123";
+        String username = "test123";
 
         // when
         boolean isActual = Pattern.matches(USER_NAME_REGEXP, username);
@@ -47,11 +48,12 @@ public class RegexTest {
         // then
         Assertions.assertTrue(isActual);
     }
-    @DisplayName("유저 비밀번호 정규식 길이 검사 실패")
+
+    @DisplayName("유저 비밀번호 길이가 정규식에 패턴에 부합하지 않아 검사에 실패한다.")
     @Test
     void regexUserPasswordOfLengthFailTest() {
         // given
-        String password = "abc123";
+        String password = "test123";
 
         // when
         boolean isActual = Pattern.matches(USER_PASSWORD_REGEXP, password);
@@ -60,11 +62,11 @@ public class RegexTest {
         Assertions.assertFalse(isActual);
     }
 
-    @DisplayName("유저 비밀번호 정규식 특수문자 검사 실패")
+    @DisplayName("유저 비밀번호 특수문자가 정규식에 패턴에 부합하지 않아 검사에 실패한다.")
     @Test
     void regexUserPasswordOfSignFailTest() {
         // given
-        String password = "abc!!!!!!";
+        String password = "test!!!!!";
 
         // when
         boolean isActual = Pattern.matches(USER_PASSWORD_REGEXP, password);
@@ -73,7 +75,7 @@ public class RegexTest {
         Assertions.assertFalse(isActual);
     }
 
-    @DisplayName("유저 비밀번호 정규식 검사 성공")
+    @DisplayName("유저 비밀번호가 정규식에 부합해 성공한다.")
     @Test
     void regexUserPasswordSuccessTest() {
         // given
