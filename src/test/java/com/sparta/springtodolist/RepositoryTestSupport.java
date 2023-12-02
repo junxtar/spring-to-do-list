90971a5f87c3b96893b6e0107a7c9197ffa7973a
junxtar <aa1870088@naver.com>
2023-12-02 15:29:57 +0900

    test: 카드 생성일자 내림차순 조회 테스트를 위한 Audition 어노테이션 추가

--- src/test/java/com/sparta/springtodolist/RepositoryTestSupport.java
+++ src/test/java/com/sparta/springtodolist/RepositoryTestSupport.java
@@ -1,10 +1,12 @@
 package com.sparta.springtodolist;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @DataJpaTest
+@EnableJpaAuditing
 public abstract class RepositoryTestSupport {
 
 }
