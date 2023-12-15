bfb4bc92d6c7dd3f8fa440fa00b959ec0dd029cd
junxtar <aa1870088@naver.com>
2023-12-15 15:38:40 +0900

    test: RepositoryTestSupport database 반영

--- src/test/java/com/sparta/springtodolist/RepositoryTestSupport.java
+++ src/test/java/com/sparta/springtodolist/RepositoryTestSupport.java
@@ -1,11 +1,14 @@
 package com.sparta.springtodolist;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @EnableJpaAuditing
 public abstract class RepositoryTestSupport {
 
