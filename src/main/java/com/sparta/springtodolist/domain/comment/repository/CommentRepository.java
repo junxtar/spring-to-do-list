788e95710c6a5f02faf402f9b2878fb5a5699415
junxtar <aa1870088@naver.com>
2023-12-21 16:14:18 +0900

    feat: cards scheduler 구현

--- src/main/java/com/sparta/springtodolist/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/springtodolist/domain/comment/repository/CommentRepository.java
@@ -1,9 +1,10 @@
 package com.sparta.springtodolist.domain.comment.repository;
 
 
+import com.sparta.springtodolist.domain.card.entity.Card;
 import com.sparta.springtodolist.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    public void deleteByCard(Card card);
 }
---
+++ src/main/java/com/sparta/springtodolist/global/scheduler/SchedulerUtil.java
@@ -0,0 +1,35 @@
+package com.sparta.springtodolist.global.scheduler;
+
+import com.sparta.springtodolist.domain.card.entity.Card;
+import com.sparta.springtodolist.domain.card.repository.CardRepository;
+import com.sparta.springtodolist.domain.comment.repository.CommentRepository;
+import com.sparta.springtodolist.infra.s3.util.S3Util;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j(topic = "Scheduler: ")
+public class SchedulerUtil {
+
+    private final S3Util s3Util;
+    private final CardRepository cardRepository;
+    private final CommentRepository commentRepository;
+
+    @Scheduled(cron = "0 0 0 * * *") //매일 오전 0시 00분
+    public void deleteCard() {
+        List<Card> cards = cardRepository.findAll();
+        LocalDateTime date = LocalDateTime.now();
+        for (Card card : cards) {
+            if (date.isAfter(card.getModifiedAt().plusDays(90))) {
+                commentRepository.deleteByCard(card);
+                s3Util.deleteImage(card.getImageName(), S3Util.IMAGE_PATH);
+                cardRepository.delete(card);
+            }
+        }
+    }
+}
